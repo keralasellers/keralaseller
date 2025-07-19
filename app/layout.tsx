cf9f5b3907d202a646af9951eb93d4e3d762fbbe
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-07-20 00:53:54 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Kerala Sellers',
+  description: 'Kerala Sellers is a platform to connect local sellers with customers.',
+
 }
+
 
 export default function RootLayout({
   children,
